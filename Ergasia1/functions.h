ffa61db55576600ddf71619551854b615784c28a
lalou <alalousis@yahoo.com>
2019-10-15 21:32:56 +0300

    asdg

--- Ergasia1/functions.h
+++ Ergasia1/functions.h
@@ -2,3 +2,5 @@ void count_input(char[256], int *, int *);
 void save_input(char[256], struct vec **);
 float average_dist(int, int, struct vec **);
 int modulo_calc(int, int, int);
+int* factors(int m, int M, int d);
+int concut(struct **h_func h, int k);
