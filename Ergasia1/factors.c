25daaa0eaae5177a952bfa937f9eb4ee8f73c0f1
lalou <alalousis@yahoo.com>
2019-10-16 16:48:49 +0300

    spasimo

--- Ergasia1/factors.c
+++ Ergasia1/factors.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 
 int* factors(int m, int M, int d){
-	int j;
+	int j, *m_power;
 	m_power=malloc(coords*sizeof(int));
 	for(j=0; j<coords; j++){
 		m_power[j] = modulo_calc(m, (coords-1) - j, M);			// Ypologizw kai apothikeuw ola ta m^(coords-1)-j % M giati einai 128 ki 													epanaxrisopoiountai polles fores
 	}
+
+	return m_power;
 }
