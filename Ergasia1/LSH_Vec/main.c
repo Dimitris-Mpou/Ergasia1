7906e08fb8bfb4891be694a283b423bf75b0d47f
Dimitris <dimitrissbousou@gmail.com>
2019-10-19 20:34:16 +0300

    fdsafsa

--- Ergasia1/LSH_Vec/main.c
+++ Ergasia1/LSH_Vec/main.c
@@ -35,7 +35,6 @@ int main (int argc, char *argv[]){
 		strcpy(output, "output");
 	}
 	count_input(input, &vec_sum, &coords);						// Metrame to plithos twn dianusmatwn
-	printf("Vectors = %d\tCoordinates = %d\n", vec_sum, coords);
 	vectors = malloc(vec_sum*sizeof(struct vec));				// Kanoume malloc gia na ta apothikeusoume
 	for(i=0; i<vec_sum; i++){
 		vectors[i].coord = malloc(coords*sizeof(int));
@@ -43,7 +42,6 @@ int main (int argc, char *argv[]){
 	save_input(input, vectors);									// Apothikeuoume ta dianusmata
 
 	count_input(query, &quer_sum, &coords);						// Metrame to plithos twn queries
-	printf("Queries = %d\tCoordinates = %d\n", quer_sum, coords);
 	queries = malloc(quer_sum*sizeof(struct vec));				// Kanoume malloc gia na ta apothikeusoume
 	for(i=0; i<quer_sum; i++){
 		queries[i].coord = malloc(coords*sizeof(int));
