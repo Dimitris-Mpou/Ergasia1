68dbdb557bae16be63da287068ddc508e407347d
lalou <alalousis@yahoo.com>
2019-10-24 16:33:28 +0300

    dtw

--- Ergasia1/LSH_Curves/dtw.c
+++ Ergasia1/LSH_Curves/dtw.c
@@ -1,29 +1,34 @@
-void dtw(struct curve a, struct curve b){
+#include <stdlib.h>
+#include <math.h>
+
+int dtw(struct curve a, struct curve b){
 	int i, j;
 	struct dwt_cell **dtw_table, min;
 	
-	dtw_table = malloc(a.noPoints+1 * sizeof(dwt_cell));
-	for(i=0; i<a.noPoints+1; i++){
-		dtw_table[i] = malloc(b.noPoints+1 * sizeof(dwt_cell));
-		for(j=0; j<b.noPoints+1; j++){
+	dtw_table = malloc(a.noPoints * sizeof(dwt_cell));
+	for(i=0; i<a.noPoints; i++){
+		dtw_table[i] = malloc(b.noPoints * sizeof(dwt_cell));
+		for(j=0; j<b.noPoints; j++){
 			dtw_table[i][j].previous = NULL; 
 	}
 
-	for(i=0; i<a.noPoints+1; i++){
-		dtw_table[i][[0].value = INFINITY;
+	for(i=0; i<a.noPoints; i++){								//Arxikopoioume thn prwth sthlh
+		dtw_table[i][[0].value = euclidean(a.points[i], b.points[0]);
 	}
 	
-	for(j=0; j<b.noPoints+1; j++){
-		dtw_table[0][[j].value = INFINITY;
+	for(j=0; j<b.noPoints; j++){
+		dtw_table[0][[j].value = euclidean(a.points[0], b.points[j]);;			//Arxikopoioume thn prwth grammh
 	}
 	
-	for(i=1; i<a.noPoints+1; i++){
-		for(j=1; j<b.noPoints+1; j++){
+	for(i=1; i<a.noPoints; i++){
+		for(j=1; j<b.noPoints; j++){
 			min = min_neighbour(dtw_table[i-1][j], dtw_table[i-1][j-1], dtw_table[i][j-1]);
-			dtw_table[i][j].value = min.value + (a.points[i].x - b.points[i].x) + (a.points[i].y - b.points[i].y);
+			dtw_table[i][j].value = min.value + euclidean(a.points[i], b.points[i]);
 			dtw_table[i][j].previous = min;
 		}
 	}
+
+	return dtw[a.noPoints-1][b.noPoints-1];
 }
 	
 struct dtw_cell min_neighbour(struct dtw_cell a, struct dtw_cell b, struct dtw_cell c){
@@ -44,7 +49,10 @@ struct dtw_cell min_neighbour(struct dtw_cell a, struct dtw_cell b, struct dtw_c
 	}
 }
 
-backtrack(struct dtw_cell **dtw_table){
+double euclidean(struct point a, struct point b){
+	return pow(pow(a.x - b.x, 2) + pow(a.y - b.y, 2), 1/2);
+}
+
 
  
 
--- Ergasia1/LSH_Curves/functions.h
+++ Ergasia1/LSH_Curves/functions.h
@@ -1,5 +1,6 @@
 int count_curves(char *);
 void count_points(char *, struct curve *);
 void save_input(char *, struct curve *);
-void dtw(struct curve, struct curve);
+int dtw(struct curve, struct curve);
 struct dtw_cell min_neighbour(struct dtw_cell, struct dtw_cell, struct dtw_cell);
+double euclidean(struct point, struct point);
