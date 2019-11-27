2f203eff129c5f07af1c7e388e7774bf4fbefc06
Dimitris <dimitrissbousou@gmail.com>
2019-11-27 18:45:49 +0200

    update a la emiris

--- Ergasia2/assignment.c
+++ Ergasia2/assignment.c
@@ -10,15 +10,15 @@ void Lloyds_assignment(struct vec *vectors, int *centers, int vec_sum, int coord
 		min_dist = 10000000.0;		
 		for(j=0; j<k; j++){
 			if( manhattan_distance(vectors[i], vectors[centers[j]], coords) < min_dist ){
-				vectors[i].nearest_centroid = centers[j];
+				vectors[i].nearest = j;
 				min_dist = manhattan_distance(vectors[i], vectors[centers[j]], coords);
 			}
 		}
 
 		min_dist = 10000000.0;
 		for(j=0; j<k; j++){
-			if( manhattan_distance(vectors[i], vectors[centers[j]], coords) < min_dist && vectors[i].nearest_centroid != centers[j]){
-				vectors[i].second_nearest = centers[j];
+			if( manhattan_distance(vectors[i], vectors[centers[j]], coords) < min_dist && vectors[i].nearest != j){
+				vectors[i].second_nearest = j;
 				min_dist = manhattan_distance(vectors[i], vectors[centers[j]], coords);
 			}
 		}
