5fb07a012dde4ff2b7788180d3712d172ef65b20
Dimitris <dimitrissbousou@gmail.com>
2020-01-15 17:41:41 +0200

    erwtima2 completed

--- Ergasia3/erwtima2.py
+++ Ergasia3/erwtima2.py
@@ -1,5 +1,6 @@
 from keras.models import load_model
 import pandas as pd
+import numpy as np
 
 from tensorflow.keras import backend as K
 
@@ -9,12 +10,11 @@ data = pd.read_csv('nn_representations.csv', usecols = [i for i in range(1,129)]
 actual = pd.read_csv('actual.csv', usecols = [i for i in range(1,8)], header=None)
 result = model.predict(data, batch_size=32)
 
+get_layer_output = K.function(inputs = model.layers[0].input, outputs = model.layers[0].output)	# create a Keras function to get layer[0]
+layer_output = get_layer_output(data)		# extract output
 
-# create a Keras function to get layer[0]
-get_layer_output = K.function(inputs = model.layers[0].input, outputs = model.layers[0].output)
+df1 = pd.read_csv('actual.csv', usecols=[0] , header=None)
+df2 = pd.DataFrame(layer_output)
 
-# extract output
-layer_output = get_layer_output(data)
-
-print(layer_output.shape)
-print(layer_output)
+out = pd.concat([df1, df2], axis=1, ignore_index=True)
+out.to_csv('new_representation.csv', index=False)
